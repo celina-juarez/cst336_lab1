1a6b757100849878972eb9206d37bda28b522215
Celina Juarez <cejuarez@csumb.edu>
2018-02-01 01:33:51 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        
+        <meta charset = "utf-8"/>
+        <title>Celina Juarez: Personal Website</title>
+        <link href = "css/style.css" rel = "stylesheet" type = "text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        <header>
+            <h1> Celina Juarez</h1>
+        </header>
+        
+        <nav>
+            <hr width = "50%"/>
+            
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <main>
+            <figure id = "me">
+                <img src = "img/juan_doe.png">
+            </figure>
+            <div = "welcomeText">
+                
+                <p>
+                Thank you for visiting my professional portfolio website.
+            </p>
+            <p>
+                I am a Software Engineer.
+            </p>
+            <p>
+                Feel free to contact me.
+        
+                
+            </p>
+            </div>
+            
+            
+        </main>
+        <footer>
+            
+            <img src="https://d36m266ykvepgv.cloudfront.net/uploads/media/iTZEL2uY2U/s-270-153/csumb.jpg"></img>
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
