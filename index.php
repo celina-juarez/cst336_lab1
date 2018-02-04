a8661cc3c78f32a39b02a6f356769128a2748c3b
Celina Juarez <cejuarez@csumb.edu>
2018-02-04 18:31:37 +0000

    Updated

--- index.php
+++ index.php
@@ -12,53 +12,63 @@ in html
 <!-- This is the head -->
 <!-- All styles and javascript go inside the head -->
     <head>
+        <meta charset = "utf-8" />
+        <title> Celina Juarez: Personal Website </title>
         
-        <meta charset = "utf-8"/>
-        <title>Celina Juarez: Personal Website</title>
-        <link href = "css/style.css" rel = "stylesheet" type = "text/css" />
+        <style>
+            body{
+                background-color: #388BB5     !important;
+            }
+        </style>
+        
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
     </head>
 <!-- closing head -->
 
     <!-- This is the body -->
     <!-- This is where we place the content of our website -->
-    <body>
+    <body style = "background-color:black; color:white">
         
         <header>
-            <h1> Celina Juarez</h1>
+            <h1> Celina Juarez </h1>
         </header>
         
         <nav>
-            <hr width = "50%"/>
-            
-            <a href="index.html">Home</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact</a>
+            <hr width = "50%" />
+            <a href = "index.html">Home</a>
+            <a href = "about.html">About</a>
+            <a href = "contact.html">Contact</a>
         </nav>
         
-        <main>
-            <figure id = "me">
-                <img src = "img/juan_doe.png">
-            </figure>
-            <div = "welcomeText">
-                
-                <p>
-                Thank you for visiting my professional portfolio website.
-            </p>
-            <p>
-                I am a Software Engineer.
-            </p>
-            <p>
-                Feel free to contact me.
+        <br /><br />
         
-                
-            </p>
+        <main>
+            
+            <figure id="me">
+                <img src = "img/celina-juarez.jpg" alt = "Picture of Celina Juarez" />
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br/>
+                <p> Thank you for visiting my professioal portfolio website.</p>
+                <p> I am a student at California State University, Monterey Bay.</p>
+                <p> I am majoring in Computer Science with an emphasis in Software Engineering.</p>
+                <p> Feel free to contact me!</p>
+                <em> "Put your heart, mind, and soul into even your smallest acts. This is the secret of success."</em><br />
+                - Swami Sivananda
             </div>
-            
-            
         </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
         <footer>
-            
-            <img src="https://d36m266ykvepgv.cloudfront.net/uploads/media/iTZEL2uY2U/s-270-153/csumb.jpg"></img>
+            <img src = "http://www.gannett-cdn.com/-mm-/9065941e142eb769bb76794c742e08d1e14ee558/r=300/http/www.gannett-cdn.com/-mm-/9065941e142eb769bb76794c742e08d1e14ee558/r=300/http/www.gannett-cdn.com/-mm-/c05fc816d491fb035461ae13eef58f569488979d/c=0-0-750-750/local/-/media/2015/05/04/Salinas/Salinas/635663646076214526-CSUMB-Athletics-logo.png" alt = "CSUMB logo" width = 100 height = 100/>
+
+            <hr>
+            CST 336. 2018&copy; Juarez <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitious.<br />
+            It is used for academic purposes only;
             
         </footer>
         <!-- closing footer -->
